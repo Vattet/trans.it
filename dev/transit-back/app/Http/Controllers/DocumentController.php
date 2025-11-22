2c27859f161467e2bfd18571b25d1a9e9a5bb779
Vattet <vadevetter@yahoo.fr>
2025-11-22 16:31:14 +0100

    api finish

--- dev/transit-back/app/Http/Controllers/DocumentController.php
+++ dev/transit-back/app/Http/Controllers/DocumentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Helpers\DocumentHelper;
+use App\Models\Document;
 use OpenApi\Attributes as OA;
 
 class DocumentController extends Controller
