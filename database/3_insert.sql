fced70b659d452b6ec1ab5d3bf32ea62d6c30fac
Vattet <vadevetter@yahoo.fr>
2025-11-25 01:09:04 +0100

    add admin password admin

--- database/3_insert.sql
+++ database/3_insert.sql
@@ -24,3 +24,26 @@ VALUES (
         '2025-11-24 14:47:28'
     );
 
+INSERT INTO
+    User (
+        ID,
+        Nom,
+        Prenom,
+        Email,
+        Mot_de_passe,
+        IsActive,
+        IsAdmin,
+        Date_Update,
+        Date_Creation
+    )
+VALUES (
+        13,
+        'admin',
+        'admin',
+        'admin@transit.it',
+        '$2y$12$cOki4OkG8fxsQRmyj2y5CeweApmeIUrAeMGXtHlJnzyElvPWRNKMy',
+        1,
+        1,
+        '2025-11-24 14:47:28',
+        '2025-11-24 14:47:28'
+    );
